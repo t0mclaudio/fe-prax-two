20d9da8a638e0da3f7e2b3ebb3ce14e0d36fa1dd
Tom Claudio <dev.tomclaudio@gmail.com>
2021-12-31 12:52:35 +0800

    add sentry for monitoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.module.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import AppContext from "./context";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import App from "./App";
+import AppContext from "./context";
+import reportWebVitals from "./reportWebVitals";
+import "./index.module.scss";
+
+// Only use in production
+process.env.NODE_ENV.production &&
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.2,
+  });
 
 ReactDOM.render(
   <React.StrictMode>
